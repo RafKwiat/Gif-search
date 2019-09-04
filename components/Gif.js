c8e37f9e2b6350ae470a9c959be127f22c452185
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-09-04 12:54:11 +0200

    Add data

--- components/Gif.js
+++ components/Gif.js
@@ -8,6 +8,7 @@ var styles = {
 Gif = React.createClass({
     getUrl: function() {
         return this.props.sourceUrl || GIPHY_LOADING_URL;
+        
     },
     render: function() {
         var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
@@ -15,9 +16,10 @@ Gif = React.createClass({
         return (
             <div style={styles}>
                 <a href={this.getUrl()} title='view this on giphy' target='new'>
-                <img id='gif' src={url} style={{width: '100%', maxWidth: '350px'}}/>
+                    <img id='gif' src={url} style={{width: '100%', maxWidth: '350px'}}/>
                 </a>
             </div>
         );
     }
 });
+
